dd66e9b102862f5f835d18ad284dd622781284b6
lokeshkharkwal7 <lokeshkharkwal@gmail.com>
2024-02-03 16:05:09 +0530

    V2

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 // document.addEventListener('DOMContentLoaded', function() { 
     const navBarLinks = document.querySelectorAll('.nav-link');
     for(let link of navBarLinks){
-      if (link.getAttribute('href') === document.URL.slice(21)){
+      if (link.getAttribute('href') === document.URL.slice(41)){
         // link.classList.remove('nav-link')
         link.classList.add('bg-info')
         link.classList.add('pt-2')
